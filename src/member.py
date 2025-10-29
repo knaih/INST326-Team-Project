31d10a448a12efb1beedfd5fd29b62d1d5224c61
AnyiT02 <atasong@terpmail.umd.edu>
2025-10-29 13:24:16 -0400

    Update member.py

--- src/member.py
+++ src/member.py
@@ -1,1 +1,12 @@
+class Member:
+  def__init__(self, member_id, name, status="active"):
+    self._if = member_id
+    self._name = name
+    self._status = status
+    self._borrowed_books = []
 
+  def is_active(self):
+    return self._status == "active"
+
+  def notify_overdue_books(self, overdue_books):
+    pass 
