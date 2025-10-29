760a5a81e8d557243af0869774e0679a256f50a8
Kaelyn Naih <kaelyncollegebound@gmail.com>
2025-10-29 13:40:18 -0400

    Refactor Library class methods and comments
    Refactor comments and improve method structure.

--- src/library.py
+++ src/library.py
@@ -1,19 +1,15 @@
 class Library:
-  
+
     def __init__(self):
-        # List to store all books
-        self._inventory = []
-        # List to store all loan records
-        self._records = []
+        self._inventory = []  # List of LibraryItem objects
+        self._records = []    # List of Loan objects (to track issued books)
 
     def add_book(self, book: LibraryItem):
-        # Add a book to the inventory
         if not isinstance(book, LibraryItem):
             raise ValueError("Can only add LibraryItem objects")
         self._inventory.append(book)
 
     def remove_book(self, isbn: str) -> bool:
-        # Remove a book by ISBN
         for book in self._inventory:
             if book.isbn == isbn:
                 self._inventory.remove(book)
@@ -21,9 +17,12 @@ class Library:
         return False
 
     def search_books(self, keyword: str) -> list:
-        # Search for books by title or author (case-insensitive)
         keyword = keyword.lower()
-        return [
-            book for book in self._inventory
-            if keyword in book.title.lower() or keyword in book.author.lower()
-        ]
+        return [book for book in self._inventory
+                if keyword in book.title.lower() or keyword in book.author.lower()]
+
+    def total_books(self) -> int:
+        return len(self._inventory)
+
+    def all_titles(self) -> list:
+        return [book.title for book in self._inventory]
