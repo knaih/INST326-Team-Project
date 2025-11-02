ea4b32d14a93743c68c710e2f28743e5a97b941b
vmukenyi <vmukenyi@terpmail.umd.edu>
2025-11-02 17:01:22 -0500

    Refactor Catalog class with proper properties

--- src/catalog.py
+++ src/catalog.py
@@ -1,3 +1,37 @@
 class Catalog:
+    @property
     def __init__(self):
-        self._items[]
+        self._inventory[]
+        
+    @property   
+    def __init__(self, name):
+        self._name = name
+        self._inventory = []
+
+    @property
+    def name(self):
+        #name
+        return self._name
+
+    @property
+    def inventory(self):
+        return list(self._inventory)
+
+    @property
+    def total_books(self):
+        return total_books(self._inventory)
+        
+        #instance methods
+    def search(search, query):
+        return search_books(self._inventory, query)
+
+    def remove_book(self, isbn):
+        remove_books(self._inventory, isbn)
+
+    def add_book(self, isbn, title, author):
+        isbn = isbn.strip()
+        title = title.strip()
+        author = author.strip()
+        book_entry = {"isbn": isbn, "title": title, "author": author}
+        add_books(self._inventory, book_entry)
+        
