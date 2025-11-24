9d13292f14f534ab90c6b301c75b4c617b38b8a6
AnyiT02 <atasong@terpmail.umd.edu>
2025-11-23 23:00:23 -0500

    Implement Member class with loan management methods

--- src/member.py
+++ src/member.py
@@ -10,3 +10,18 @@ class Member:
 
   def notify_overdue_books(self, overdue_books):
     pass 
+
+
+from loan import Loan
+
+class Member:
+    def __init__(self, name, member_id):
+        self.name = name
+        self.member_id = member_id
+        self.loans = []     # composition → Member HAS-A list of Loans
+
+    def add_loan(self, loan):
+        self.loans.append(loan)
+
+    def remove_loan(self, loan):
+        self.loans.remove(loan)
